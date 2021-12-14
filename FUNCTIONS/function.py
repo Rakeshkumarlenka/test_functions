104ca9493db51b2800b505f25e4b8333376eb91b
rakeshkumarlenka <rakeshkumar.lenka@modernchipsolutions.com>
2021-12-14 11:26:37 +0530

    11-12-2021

---
+++ FUNCTIONS/function.py
@@ -0,0 +1,15 @@
+class function_prob():
+    def convert(self):
+        list1 = [1, 2, 3, 4]
+        print( tuple(list1))
+        my_set = {'ram','is','a','good','boy'}
+        s = list(my_set)
+        print(s)
+        t = ('x', 'y', 'z')
+        print(set(t))
+
+
+
+
+res = function_prob()
+res.convert()
